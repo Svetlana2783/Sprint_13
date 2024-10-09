3038fbc03fd238086df9381dcaa3bb994bc62d9e
Svetlana Shaykhlislamova <svetlana2783@list.ru>
2024-10-10 00:16:30 +0300

    Первый коммит в 13 спринте

---
+++ data.py
@@ -0,0 +1,33 @@
+class Users:
+    data_current = {
+        "login": "sdfghjkjhgfds",
+        "password": "12344567890"
+    }
+    data_negative = {
+        "login": "dfghjkjhgfds",
+        "password": "2344567890"
+    }
+    data_without_login = {
+        "login": "",
+        "password": "12344567890"
+    }
+    data_without_password = {
+        "login": "sdfghjkjhgfds",
+        "password": ""
+    }
+
+
+class Orders:
+    data_order = {
+        "firstName": "Svetlana",
+        "lastName": "Shaykhlislamova",
+        "address": "SPb, Mira, 1",
+        "metroStation": 3,
+        "phone": "+7 987 654 32 10",
+        "rentTime": 3,
+        "deliveryDate": "2024-10-08",
+        "comment": "Privet, Courier",
+        "color": [
+            "BLACK"
+        ]
+    }
